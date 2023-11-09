7e5c7dca6fcb0f1b29a9842b723b3adf020020aa
Sabır Akbana <s.akbana9o@gmail.com>
2023-11-09 17:34:58 +0300

    11 Kasım Ders Sonu

---
+++ OOP2/Program.cs
@@ -0,0 +1,18 @@
+﻿using OOP2;
+
+GercekMusteri musteri1 = new();
+musteri1.MusteriNo = "";
+
+TuzelMusteri musteri2 = new();
+musteri2.MusteriNo = "";
+
+
+
+Musteri musteri3 = new GercekMusteri();
+Musteri musteri4 = new TuzelMusteri();
+
+MusteriManager musteriManager = new MusteriManager();
+musteriManager.Ekle(musteri1);
+musteriManager.Ekle(musteri2);
+musteriManager.Ekle(musteri3);
+musteriManager.Ekle(musteri4);
---
+++ OOP3/Program.cs
@@ -0,0 +1,22 @@
+﻿using OOP3;
+using System.Collections.Generic;
+
+IKrediManager tasitKrediManager = new TasitKrediManager();
+IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+IKrediManager konutKrediManager = new KonutKrediManager();
+
+
+
+BasvuruManager basvuruManager = new BasvuruManager();
+basvuruManager.BasvuruYap(tasitKrediManager, new List<ILoggerService> {new DatabaseLoggerService(), new FileLoggerService() });
+
+
+List<IKrediManager> krediler = new() { tasitKrediManager, ihtiyacKrediManager, konutKrediManager };
+basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+ILoggerService databaseLogger = new DatabaseLoggerService();
+ILoggerService fileLogger = new FileLoggerService();
+
+List<ILoggerService> loggerServices = new() { databaseLogger, fileLogger };
+
+basvuruManager.BasvuruYap(konutKrediManager, loggerServices);
